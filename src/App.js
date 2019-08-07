14080ae3c391147033c34fdf1a214a3a6c8a8b55
Chris <chrislo.7@live.com>
2019-08-07 19:07:19 -0400

    displaying states utilizing useSelector from react-redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+
   return (
     <div className="App">
       <h1>Hello</h1>
+      <div>
+        <h2>Counter State = { counter }</h2>
+        { isLogged ? <h3>Logged in text</h3> : <h3>You are not logged in!</h3> }
+      </div>
     </div>
   );
 }
